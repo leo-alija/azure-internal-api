093467085a466043ca501365cd2013c2070b58b8
leo-alija <leo.alija@hotmail.com>
2026-03-11 10:25:44 +0000

    Adding the alert rule

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -52,3 +52,27 @@ module "function_app" {
   tags                           = local.tags
 }
 
+# Alert Rule
+# This is added here because it needs the Function App ID, which doesn't exist until after the function module runs.
+resource "azurerm_monitor_metric_alert" "func_5xx" {
+  name                = "alert-5xx-${local.name_prefix}"
+  resource_group_name = module.networking.resource_group_name
+  scopes              = [module.function_app.function_app_id]
+  description         = "Fires when Function App returns 5+ HTTP 5xx errors in 5 minutes"
+  severity            = 2
+  frequency           = "PT1M" # Every 1 minute
+  window_size         = "PT5M" # Every 5 minutes
+  tags                = local.tags
+
+  criteria {
+    metric_namespace = "Microsoft.Web/sites"
+    metric_name      = "Http5xx"
+    aggregation      = "Total"
+    operator         = "GreaterThanOrEqual"
+    threshold        = 5
+  }
+
+  action {
+    action_group_id = module.observability.action_group_id
+  }
+}
